4cfd9a57271a2ce4aaecdda28d2ccc88df5b6e80
Polina Tutygina <polina.tutygina@yandex.ru>
2025-01-05 17:05:58 +0300

    Kittygram_v1.0

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,5 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
+from rest_framework.authtoken import views
+
 from django.urls import include, path
 
 from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
@@ -12,4 +14,12 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
+    
+    # аутентификацию через Authtoken
+    #path('api-token-auth/', views.obtain_auth_token),
 ] 
